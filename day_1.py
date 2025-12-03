ade6bfc3b560446b92569e026a2a12d8e3089fda
d050150 <tibor.tarnai@sap.com>
2025-12-03 17:00:05 +0100

    day 1 #2

--- day_1.py
+++ day_1.py
@@ -1,13 +1,22 @@
-current, password = 50, 0
+current, password, x434 = 50, 0, 0
 
 with open('day_1.txt') as ifile:
     for line in ifile:
         if line:
+            rotations = int(line[1:])
+            x434 += rotations // 100
+            rotations %= 100
             if line[0] == "R":
-                current = (current + int(line[1:])) % 100
+                if current + rotations > 100:
+                    x434 += 1
+                current = (current + rotations) % 100
             elif line[0] == "L":
-                current = (current - int(line[1:])) % 100
+                if current != 0 and current - rotations < 0:
+                    x434 += 1
+                current = (current - rotations) % 100
+
             if current == 0:
                 password += 1
 
 print(password)
+print(password + x434)
